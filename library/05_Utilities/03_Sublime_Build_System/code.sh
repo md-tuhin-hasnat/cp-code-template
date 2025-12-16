a5582fe32f41d16ff2286798aeed5fa4d4f953de
md-tuhin-hasnat <mdtuhinhasnat@outlook.com>
2025-12-16 21:45:56 +0600

    Fix: Use shell_cmd for proper shell operator interpretation

--- library/05_Utilities/03_Sublime_Build_System/code.sh
+++ library/05_Utilities/03_Sublime_Build_System/code.sh
@@ -1,8 +1,5 @@
 {
-    "cmd": ["g++", "-std=c++17", "${file}",
-            "-o", "${file_base_name}",
-            "&&", "./${file_base_name}<inputf.in>outputf.out"],
-    "shell": true,
-    "working_dir": "$file_path",
+    "shell_cmd": "g++ -std=c++17 \"${file}\" -o \"${file_path}/${file_base_name}\" && \"${file_path}/${file_base_name}\" < \"${file_path}/inputf.in\" > \"${file_path}/outputf.out\"",
+    "working_dir": "${file_path}",
     "selector": "source.cpp"
 }
